d28e99601c18da6d6c4394f93da7132bed234dd1
thomp2e3 <thomp2e3@mail.uc.edu>
2024-10-16 17:47:07 -0400

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Scanner console = new Scanner(System.in);
+
+        String partyAfil = "";
+
+
+        System.out.println("Enter your party affiliation [RDI]: ");
+
+        {
+            partyAfil = console.nextLine();
+        }
+        if (partyAfil.equalsIgnoreCase("R")) {
+            System.out.println("You get a Republician Elephant!!");
+        }
+        else if (partyAfil.equalsIgnoreCase("D"))
+        {
+            System.out.println("You get a Democrat Donkey!!");
+        }
+        else if (partyAfil.equalsIgnoreCase("I"))
+        {
+            System.out.println("You get an independent man!!");
+       }
+        else
+            System.out.println("I don't know what party you are in, rerun the program...");
     }
 }
